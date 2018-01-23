a91bf3b5767f14bde2d608bc50c20805b7835ba2
Amir <Amir.Aavani@gmail.com>
2018-01-22 23:03:32 -0800

    Codeforces ...

--- Codeforces/Problemset/P-158A.pp
+++ Codeforces/Problemset/P-158A.pp
@@ -1,25 +1,32 @@
 program P158A;
-uses
-  fgl;
 var
   n, k: Integer;
   
 function ReadAndSolve: Integer;
 var
-  i, x: Integer;
+  i, x, y: Integer;
 
 begin
   ReadLn(n, k);
   Result := 0;
 
-  for i := 1 to n do
+  for i := 1 to k do
   begin
     Read(x);
-    if k < x then
+    if x = 0 then
+      Exit(i - 1);
+  end;
+
+  y := x;
+  Result := k;
+  for i := k + 1 to n do
+  begin
+    Read(x);
+    if x = y then
       Inc(Result)
     else
       break;
-  end;   
+  end;
 end;
 
 begin
